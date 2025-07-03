15fdd38355988a8040a3740b175058cc87362b82
Alan Curtis <alancurtis5000+accounts@gmail.com>
2025-07-02 23:49:23 -0600

    adding monkey type test

---
+++ src/monkeytype-test.js
@@ -0,0 +1,47 @@
+const singles = 'singles'
+const doubles = "doubles"
+const backtick = `backtick`
+
+// code 
+const blockedComment = "/*comment*/"
+const singleComment = "//"
+const variable = "variable"
+const string = "string"
+const arrowFunction = () => { }
+const templateString = `this has a ${variable}`
+const array = []
+const object = {}
+
+// math
+const a = 1
+const b = 2
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const add = (a + b)
+const minus = (a - b)
+const multiply = (a * b)
+const divide = (a / b)
+const modulas = (a % b)
+
+// comparisions
+const and = a && b
+const or = a || b
+const equal = a === b
+const greaterThan = a > b
+const lessThan = a < b
+
+// symbols
+const symbols = `
+!@#$%
+\/*-+
+?"'~
+_
+:;&|=
+<({[,
+>)}].
+^
+`
+
+// casing
+const pascal = 'thisIsPascalCase'
+const kabob = 'this-is-kabob-case'
+const macro = 'THIS_IS_MACRO_CASE'
